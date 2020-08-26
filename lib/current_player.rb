c22085a83d0e82d89e810abfb8cc2be730c6338e
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-08-26 01:42:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,8 @@
 def turn_count(board)
   i = 0
   board.each do |count|
-    if count = "X" || count = "O"
+    if count == "X" or  count == "O"
       i += 1
-    else
-      i
     end
   end
 end
