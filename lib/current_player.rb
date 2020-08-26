19a468275c61ae9a82a1073e2f4545d9fab7a057
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-08-26 01:54:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,13 +8,6 @@ def turn_count(board)
   return i
 end
 
-def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
-end
-
 def current_player
-  turn_count(board) 
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
